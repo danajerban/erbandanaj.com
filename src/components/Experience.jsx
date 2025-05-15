35bd785a6ee3f413ee3fa119ff6e7ed208ebf71d
Erban Danaj <danajerban@gmail.com>
2025-05-15 05:17:09 +0200

    feat: contact section initial details

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -19,6 +19,10 @@ import { BookCase } from "./BookCase";
 import * as THREE from "three";
 import { config } from "../config";
 import { Monitor } from "./Monitor";
+import { Balloon } from "./Balloon";
+import { Mailbox } from "./Mailbox";
+import { Pigeon } from "./Pigeon";
+import { ParkBench } from "./ParkBench";
 
 const SECTIONS_DISTANCE = 10;
 
@@ -141,7 +145,48 @@ export const Experience = () => {
         </group>
         {/* CONTACT */}
         <group position-z={SECTIONS_DISTANCE * 3}>
-          <SectionTitle position-x={0.5}>Contact</SectionTitle>
+          <SectionTitle position-x={-2} position-z={0.6}>
+            Contact
+          </SectionTitle>
+          <group position-x={-2}>
+            <ParkBench
+              scale={0.6}
+              position-x={-0.5}
+              position-z={-2.5}
+              rotation-y={-Math.PI / 4}
+            />
+            <group position-y={2.2} position-z={-0.5}>
+              <Float floatIntensity={2} rotationIntensity={1.5}>
+                <Balloon scale={1.5} position-x={-0.5} color="#71a2d9" />
+              </Float>
+              <Float
+                floatIntensity={1.5}
+                rotationIntensity={2}
+                position-z={0.5}
+              >
+                <Balloon scale={1.3} color="#d97183" />
+              </Float>
+              <Float speed={2} rotationIntensity={2}>
+                <Balloon scale={1.6} position-x={0.4} color="yellow" />
+              </Float>
+            </group>
+          </group>
+
+          <Mailbox
+            scale={0.25}
+            rotation-y={1.25 * Math.PI}
+            position-x={1}
+            position-y={0.25}
+            position-z={0.5}
+          />
+          <Float floatIntensity={1.5} speed={3}>
+            <Pigeon
+              position-x={2}
+              position-y={1.5}
+              position-z={-0.5}
+              scale={0.2}
+            />
+          </Float>
         </group>
       </group>
     </>
