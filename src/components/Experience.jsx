44b6d0c66602195b696b86da5fbbddb58f728bc2
Erban Danaj <danajerban@gmail.com>
2025-05-15 02:30:33 +0200

    components: personal avatar

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,10 @@
 import { Environment } from "@react-three/drei";
-
+import { Avatar } from "./Avatar";
 export const Experience = () => {
   return (
     <>
       <Environment preset="sunset" />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial color="white" />
-      </mesh>
+      <Avatar />
     </>
   );
 };
