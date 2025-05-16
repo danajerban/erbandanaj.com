45b6386cfd88785f8e154125ff0fce114502b09d
Erban Danaj <danajerban@gmail.com>
2025-05-16 17:42:56 +0200

    style: colors tweak

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -4,7 +4,7 @@ export const SectionTitle = ({ children, ...props }) => {
   return (
     <Text3D font={"fonts/Inter_Bold.json"} size={0.3} {...props}>
       {children}
-      <meshStandardMaterial color="white" />
+      <meshStandardMaterial color="#FAEAEA" />
     </Text3D>
   );
 };
