9c6dd3c257fbbce08df8c8239b76e4960c017601
Erban Danaj <danajerban@gmail.com>
2025-05-16 05:45:47 +0200

    config: core skills w/ icons

--- src/config.js
+++ src/config.js
@@ -2,8 +2,25 @@ export const config = {
   sections: ["home", "skills", "projects", "contact"],
   home: {
     title: "Erban",
-    subtitle: "Danaj",
+    subtitle: "DANAJ",
   },
+  skills: [
+    {
+      name: "React",
+      icon: "icons/react-native.png",
+      level: "80",
+    },
+    {
+      name: "Javascript",
+      icon: "icons/javascript.png",
+      level: "90",
+    },
+    {
+      name: "Three.js",
+      icon: "icons/threejs.png",
+      level: "30",
+    },
+  ],
   contact: {
     name: "Erban Danaj",
     address: "Warsaw, Poland",
