abaf4651c30c4327d142ea26371c441852f065ec
Erban Danaj <danajerban@gmail.com>
2025-05-16 17:42:21 +0200

    style: mobile responsive

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -4,17 +4,17 @@ import { motion } from "framer-motion";
 import { atom, useAtom } from "jotai";
 import { useState } from "react";
 import { config } from "../config";
+import { useMobile } from "../hooks/useMobile";
 
 export const projectAtom = atom(config.projects[0]);
-
 export const Interface = () => {
-  const [hasScrolled, setHasScrolled] = useState(false);
   const scrollData = useScroll();
+  const [hasScrolled, setHasScrolled] = useState(false);
   useFrame(() => {
-    setHasScrolled(scrollData.offset > 0);
-  }); // its either true or false because normally we dont use state in useFrame to avoid re-renders
+    setHasScrolled(scrollData.offset > 0); // its either true or false because normally we dont use state in useFrame to avoid re-renders
+  });
   const [_project, setProject] = useAtom(projectAtom);
-
+  const { isMobile } = useMobile();
   return (
     <div className="interface">
       <div className="sections">
@@ -22,76 +22,87 @@ export const Interface = () => {
         <section className="section section--bottom">
           <motion.div
             className="scroll-down"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: hasScrolled ? 0 : 1 }}
-            transition={{ duration: 0.5 }}
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: hasScrolled ? 0 : 1,
+            }}
           >
             <motion.div
               className="scroll-down__wheel"
-              initial={{ translateY: 0 }}
-              animate={{ translateY: 4 }}
+              initial={{
+                translateY: 0,
+              }}
+              animate={{
+                translateY: 4,
+              }}
               transition={{
                 duration: 0.4,
                 repeatDelay: 0.5,
-                repeat: Infinity,
                 repeatType: "reverse",
+                repeat: Infinity,
               }}
             ></motion.div>
           </motion.div>
         </section>
         {/* SKILLS */}
         <section className="section section--right mobile--section--left mobile--section--bottom">
-          <motion.div className="skills">
-            <motion.div
-              className="skills__title"
-              whileInView={"visible"}
-              initial={{ opacity: 0 }}
-              variants={{
-                visible: { opacity: 1 },
-              }}
-            >
-              {config.skills.map((skill, idx) => (
-                <motion.div
-                  onMouseEnter={() => setProject(project)}
-                  key={skill.name}
-                  className="skill"
-                  initial={{ opacity: 0 }}
-                  variants={{
-                    visible: {
-                      opacity: 1,
-                    },
-                  }}
-                  transition={{
-                    duration: 1,
-                    delay: idx * 0.62,
-                  }}
-                >
-                  <div className="skill__label">
-                    <img
-                      className="skill__label__image"
-                      src={skill.icon}
-                      alt={skill.name}
-                    />
-                    <h2 className="skill__label__name">{skill.name}</h2>
-                  </div>
-                  <div className="skill__level">
-                    <motion.div
-                      className="skill__level__bar"
-                      initial={{ width: 0 }}
-                      variants={{
-                        visible: {
-                          width: `${skill.level}%`,
-                        },
-                      }}
-                      transition={{
-                        duration: 1,
-                        delay: idx * 0.62,
-                      }}
-                    ></motion.div>
-                  </div>
-                </motion.div>
-              ))}
-            </motion.div>
+          <motion.div
+            className="skills"
+            whileInView={"visible"}
+            initial={{
+              opacity: 0,
+            }}
+            variants={{
+              visible: {
+                opacity: 1,
+              },
+            }}
+            viewport={{
+              margin: isMobile ? "-70% 0px 0px 0px" : undefined,
+            }}
+          >
+            {config.skills.map((skill, idx) => (
+              <motion.div
+                key={skill.name}
+                className="skill"
+                initial={{ opacity: 0 }}
+                variants={{
+                  visible: {
+                    opacity: 1,
+                  },
+                }}
+                transition={{
+                  duration: 1,
+                  delay: isMobile ? 0 : idx * 0.62,
+                }}
+              >
+                <div className="skill__label">
+                  <img
+                    className="skill__label__image"
+                    src={skill.icon}
+                    alt={skill.name}
+                  />
+                  <h2 className="skill__label__name">{skill.name}</h2>
+                </div>
+                <div className="skill__level">
+                  <motion.div
+                    className="skill__level__bar"
+                    initial={{ width: 0 }}
+                    variants={{
+                      visible: {
+                        width: `${skill.level}%`,
+                      },
+                    }}
+                    transition={{
+                      duration: 1,
+                      delay: idx * 0.62,
+                    }}
+                  ></motion.div>
+                </div>
+              </motion.div>
+            ))}
           </motion.div>
         </section>
         {/* PROJECTS */}
@@ -107,10 +118,14 @@ export const Interface = () => {
                 opacity: 1,
               },
             }}
+            viewport={{
+              margin: isMobile ? "-70% 0px 0px 0px" : undefined,
+            }}
           >
             {config.projects.map((project, idx) => (
               <motion.div
-                key={project.name + idx}
+                onMouseEnter={() => setProject(project)}
+                key={project.name}
                 className="project"
                 initial={{ opacity: 0 }}
                 variants={{
@@ -120,7 +135,7 @@ export const Interface = () => {
                 }}
                 transition={{
                   duration: 1,
-                  delay: idx * 0.5,
+                  delay: isMobile ? 0 : idx * 0.5,
                 }}
               >
                 <a href={project.link} target="_blank">
