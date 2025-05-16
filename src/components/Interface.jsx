9b993195aa9593d06fd633ceeca7bf5507ebab3c
Erban Danaj <danajerban@gmail.com>
2025-05-16 06:23:55 +0200

    feat: projects section (wip)

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -3,10 +3,10 @@ import { useScroll } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { motion } from "framer-motion";
 import { config } from "../config";
+
 export const Interface = () => {
   const [hasScrolled, setHasScrolled] = useState(false);
   const scrollData = useScroll();
-
   useFrame(() => {
     setHasScrolled(scrollData.offset > 0);
   }); // its either true or false because normally we dont use state in useFrame to avoid re-renders
@@ -46,10 +46,67 @@ export const Interface = () => {
                 visible: { opacity: 1 },
               }}
             >
-               {config.skills.map((skill, idx) => (
+              {config.skills.map((skill, idx) => (
+                <motion.div
+                  key={skill.name}
+                  className="skill"
+                  initial={{ opacity: 0 }}
+                  variants={{
+                    visible: {
+                      opacity: 1,
+                    },
+                  }}
+                  transition={{
+                    duration: 1,
+                    delay: idx * 0.62,
+                  }}
+                >
+                  <div className="skill__label">
+                    <img
+                      className="skill__label__image"
+                      src={skill.icon}
+                      alt={skill.name}
+                    />
+                    <h2 className="skill__label__name">{skill.name}</h2>
+                  </div>
+                  <div className="skill__level">
+                    <motion.div
+                      className="skill__level__bar"
+                      initial={{ width: 0 }}
+                      variants={{
+                        visible: {
+                          width: `${skill.level}%`,
+                        },
+                      }}
+                      transition={{
+                        duration: 1,
+                        delay: idx * 0.62,
+                      }}
+                    ></motion.div>
+                  </div>
+                </motion.div>
+              ))}
+            </motion.div>
+          </motion.div>
+        </section>
+        {/* PROJECTS */}
+        <section className="section section--left mobile--section--bottom">
+          <motion.div
+            className="projects"
+            whileInView={"visible"}
+            initial={{
+              opacity: 0,
+            }}
+            variants={{
+              visible: {
+                opacity: 1,
+              },
+            }}
+          >
+            {config.projects.map((project, idx) => (
               <motion.div
-                key={skill.name}
-                className="skill"
+                key={project.name + idx}
+                className="project"
                 initial={{ opacity: 0 }}
                 variants={{
                   visible: {
@@ -58,40 +115,25 @@ export const Interface = () => {
                 }}
                 transition={{
                   duration: 1,
-                  delay: idx * 0.62,
+                  delay: idx * 0.5,
                 }}
               >
-                <div className="skill__label">
+                <a href={project.link} target="_blank">
                   <img
-                    className="skill__label__image"
-                    src={skill.icon}
-                    alt={skill.name}
+                    className="project__image"
+                    src={project.image}
+                    alt={project.name}
                   />
-                  <h2 className="skill__label__name">{skill.name}</h2>
-                </div>
-                <div className="skill__level">
-                  <motion.div
-                    className="skill__level__bar"
-                    initial={{ width: 0 }}
-                    variants={{
-                      visible: {
-                        width: `${skill.level}%`,
-                      },
-                    }}
-                    transition={{
-                      duration: 1,
-                      delay: idx * 0.62,
-                    }}
-                  ></motion.div>
-                </div>
+                  <div className="project__details">
+                    <h2 className="project__details__name">{project.name}</h2>
+                    <p className="project__details__description">
+                      {project.description}
+                    </p>
+                  </div>
+                </a>
               </motion.div>
             ))}
-            </motion.div>
           </motion.div>
-        </section>
-        {/* PROJECTS */}
-        <section className="section section--left mobile--section--bottom">
-          PROJECTS
         </section>
         {/* CONTACT */}
         <section className="section section--left mobile--section--bottom">
