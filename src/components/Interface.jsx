a7347d9525ebe0076f3e25168827ea60e00b5e26
Erban Danaj <danajerban@gmail.com>
2025-05-16 05:29:04 +0200

    feat: html interface init

---
+++ src/components/Interface.jsx
@@ -0,0 +1,22 @@
+export const Interface = () => {
+  return (
+    <div className="interface">
+      <div className="sections">
+        {/* HOME */}
+        <section className="section section--bottom">HOME</section>
+        {/* SKILLS */}
+        <section className="section section--right mobile--section--left mobile--section--bottom">
+          SKILLS
+        </section>
+        {/* PROJECTS */}
+        <section className="section section--left mobile--section--bottom">
+          PROJECTS
+        </section>
+        {/* CONTACT */}
+        <section className="section section--left mobile--section--bottom">
+          CONTACT
+        </section>
+      </div>
+    </div>
+  );
+};
